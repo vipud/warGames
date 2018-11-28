9e49fee0d67bb258f6eb5f1ab99552befe489e17
kkatayama <katayama@udel.edu>
2018-11-28 16:39:17 -0500

    init - enums

---
+++ recon/enums.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+
+class OS(Enum):
+    Windows = "Windows"
+    Linux = "Linux"
+    Other = "Other"
+
+
+class Arch(Enum):
+    x86 = "x86"
+    x64 = "x64"
+    ARM = "ARM"
+    MIPS = "MIPS"
+    Other = "Other"
+
+
+class Protocol(Enum):
+    SSH = "SSH"
+    FTP = "FTP"
+    DNS = "DNS"
+    HTTP = "HTTP"
+    RDP = "RDP"
+    XMPP = "XMPP"
+    SQL = "SQL"
+    OTHER = "OTHER"
